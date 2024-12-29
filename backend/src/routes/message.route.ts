af09c372eb61ee9832d10c2722fbe5a7b986ac26
Kabir Sharma <sharmakabir204@gmail.com>
2024-12-30 03:14:29 +0530

    fixed get-messages error

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
-
+import {protectRoute} from "../middleware/protectRoute.js";
+import { sendMessage,  getMessages  } from "../controllers/message.controller.js"
+// import {} from "../controllers/message.controller.js";
+// import sendMessage from "../controllers/message.controller.js";
 const router=express.Router();
 
-router.get("/conversations", (req,res) =>{
-    res.send("Conv route");
-});
 
 
-
+router.post("/send/:id",  protectRoute, sendMessage)
+router.get("/:id", protectRoute, getMessages);
 export default router;
