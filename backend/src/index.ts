5e4cf26ed49c1287f666a47866892b2b2a6dd2a6
Kabir Sharma <sharmakabir204@gmail.com>
2024-12-30 01:31:09 +0530

    auth routes and controllers added

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+// const a:number=1;
+// console.log(a);
+
+import express from 'express';
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+dotenv.config();
+const app=express();
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+app.listen(5005, ()=>{
+    console.log("Server is running on port 5005");
+})
+
+
---
+++ backend/src/utils/generateToken.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import {Response} from 'express';
+
+const generateToken = (userId:string, res:Response)=> {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET!, {
+        expiresIn: "7d"
+    })
+    //maxage in ms
+    res.cookie("jwt", token, {
+        maxAge: 15*24*60*60*1000, //ms
+        httpOnly: true, //prevent xss cross site scripting, not accesable by javascript!
+        sameSite: "strict", // CSRF attack cross-site request forgery it means that the cookie will only be sent in requests originating from the same site that set the cookie. This helps prevent CSRF attacks by ensuring that cookies are not sent along with cross-site requests.
+
+        /*CSRF attacks occur when a malicious website tricks a user's browser into making an unwanted request to another site where the user is authenticated. For example, if a user is logged into their bank account and visits a malicious site, the malicious site could attempt to perform actions on the bank's site usi*/
+        
+		secure: process.env.NODE_ENV !== "development", // HTTPS wehn in dev, http in localhost
+    })
+    return token;
+};
+
+export default generateToken;
