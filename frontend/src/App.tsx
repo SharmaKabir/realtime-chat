e6b20d77cafa800835e02079be14bfd5a5e175e9
Kabir Sharma <sharmakabir204@gmail.com>
2024-12-31 01:08:51 +0530

    auth context.tsx added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, Navigate} from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
+import { useAuthContext } from './context/AuthContext';
 
 function App() {
- 
-
+  const {authUser,isLoading }=useAuthContext();
+  console.log(authUser);
+  if(isLoading) return null;
   return (
     <div className="p-4 h-screen flex items-center justify-center">  
     <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/signup' element={<SignUp/>} />
-        <Route path='/login' element={<Login/>} />
+        <Route path='/' element={ authUser ? <Home/> : <Navigate to={"login"}/>} /> 
+        {/* //IF NOT AUTH IMMEDIATLEY GO TO LOGIN */}
+
+        <Route path='/signup' element={!authUser ? <SignUp/>:<Navigate to={"/"}/>} />
+        <Route path='/login' element={!authUser ?<Login/>:<Navigate to={"/"}/>} />
     </Routes>
     </div>
   )
