977830533d1f34b9ba2b8c43919c09f5370d01b6
Kabir Sharma <sharmakabir204@gmail.com>
2024-12-31 00:18:53 +0530

    added frontend components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 function App() {
  
 
   return (
-    <>  
-    <h1 className="text-red-500  TEXT">Hello</h1>
-     <button className="btn btn-primary"> my button</button>
-    </>
+    <div className="p-4 h-screen flex items-center justify-center">  
+    <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/login' element={<Login/>} />
+    </Routes>
+    </div>
   )
 }
 
